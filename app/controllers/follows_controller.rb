e3535c299313adb732fa6a3add5f9983cfe03a0f
Guilherme Arruda <guilherme_arruda@live.com>
2019-12-21 19:32:07 -0300

    Configuração das views profile, following e followers. Correçãos de alguns bugs

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,15 +1,14 @@
 class FollowsController < ApplicationController
-  load_and_authorize_resource
   before_action :set_user
   
   def create
     respond_to do |format|
       if current_user.follow(@user)
         format.json { render @user, status: :created }
-        format.html { redirect_to :back, notice: 'User followed with success.' }
+        format.html { redirect_back fallback_location: root_path, notice: 'User followed with success.' }
       else
         format.json { render json: nil, status: :unprocessable_entity }
-        format.html { redirect_to :back }
+        format.html { redirect_back fallback_location: root_path }
       end
     end
   end
@@ -18,10 +17,10 @@ class FollowsController < ApplicationController
     respond_to do |format|
       if current_user.stop_following(@user)
         format.json { head :no_content }
-        format.html { redirect_to :back, notice: 'User unfollowed with success.' }
+        format.html { redirect_back fallback_location: root_path, notice: 'User unfollowed with success.' }
       else
         format.json { render json: nil, status: :unprocessable_entity }
-        format.html { redirect_to :back }
+        format.html { redirect_back fallback_location: root_path }
       end
     end
   end
